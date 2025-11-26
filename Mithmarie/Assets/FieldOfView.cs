b6497a09c42dd0a370ae6dbae5835f7d32b20914
Dan-Mechanics <github.tim@desmidt.org>
2025-11-26 20:34:49 +0100

    move field of view mono

--- Mithmarie/Assets/FieldOfView.cs
+++ Mithmarie/Assets/FieldOfView.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 namespace Mithmarie
 {
@@ -7,35 +6,11 @@ namespace Mithmarie
     {
         [SerializeField] private Camera cam = default;
         [SerializeField] private PersistentFloat fov = default;
-        [SerializeField, Min(0f)] private float fovPerScroll = default;
-        [SerializeField] private float minValue = default;
-        [SerializeField] private float maxValue = default;
 
         public override void Enter()
         {
             base.Enter();
             fov.Load();
-            cam.fieldOfView = fov.value;
-        }
-
-        public override void OnFrame()
-        {
-            base.OnFrame();
-            float value = -Mouse.current.scroll.value.y;
-            if (value > 0f)
-            {
-                fov.value += fovPerScroll;
-            }
-            else if (value < 0f)
-            {
-                fov.value -= fovPerScroll;
-            }
-            else
-            {
-                return;
-            }
-
-            fov.value = Mathf.Clamp(fov.value, minValue, maxValue);
             cam.fieldOfView = fov.value;
         }
 
