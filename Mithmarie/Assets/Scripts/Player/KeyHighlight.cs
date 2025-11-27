6b9a0c15e94c76f041206545e0d0e30fdd8d63fb
Dan-Mechanics <github.tim@desmidt.org>
2025-11-27 11:59:12 +0100

    refactor

--- Mithmarie/Assets/Scripts/Player/KeyHighlight.cs
+++ Mithmarie/Assets/Scripts/Player/KeyHighlight.cs
@@ -12,6 +12,12 @@ namespace Mithmarie
         [SerializeField] private Image icon = default;
         [SerializeField] private TMP_Text text = default;
 
+        private void Start()
+        {
+            text.text = key.ToString().ToUpperInvariant().
+                Replace("LEFT", string.Empty).Replace("RIGHT", string.Empty); ;
+        }
+
         public void Draw(Color colorA, Color colorB)
         {
             image.color = Keyboard.current[key].isPressed ? colorB : colorA;
