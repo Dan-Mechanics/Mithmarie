13f5c893be16257e9b214c35ff8bb67c3f0de1cf
Dan-Mechanics <github.tim@desmidt.org>
2025-12-05 22:09:57 +0100

    fix hover

---
+++ Mithmarie/Assets/Scripts/Player/TerraformRaycast.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Mithmarie
+{
+    [CreateAssetMenu(menuName = "ScriptableObject/" + nameof(TerraformRaycast), fileName = "New " + nameof(TerraformRaycast))]
+    public class TerraformRaycast : ScriptableObject
+    {
+        [Min(0f)] public float maxRange;
+        public LayerMask mask;
+        public float normalOffset;
+        public float airPlacementDistance;
+
+        public bool Cast(Transform eyes, out RaycastHit hit)
+        {
+            if (Physics.Raycast(eyes.position, eyes.forward, out hit, maxRange, mask, QueryTriggerInteraction.Ignore))
+            {
+                hit.point += hit.normal * normalOffset;
+                return true;
+            }
+
+            hit.point = eyes.position + (eyes.forward * airPlacementDistance);
+            hit.normal = -eyes.forward; // INCORRECT.
+            return false;
+        }
+    }
+}
--- Mithmarie/Assets/Scripts/Player/Terraformer.cs
+++ Mithmarie/Assets/Scripts/Player/Terraformer.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 
 namespace Mithmarie
 {
-    public class Terraformer : StateBehaviour, IRaycastProvider
+    public class Terraformer : StateBehaviour
     {
         public event Action<Vector3Int?, Vector3Int?> OnShowPreview;
         public event Action<Vector3Int, Vector3Int> OnEditSelection;
@@ -12,19 +12,19 @@ namespace Mithmarie
         [SerializeField] private Transform eyes = default;
         [SerializeField] private bool leftMouseButton = default;
         [SerializeField] private PersistentBool swapMouseButtons;
-        [SerializeField] private RaycastSettings raycast = default;
+        [SerializeField] private TerraformRaycast raycast = default;
 
         private RaycastHit hit;
         private Vector3Int? firstPos;
         private Vector3Int? secondPos;
         private World world;
 
-        public class FillPreview
+        /*public class FillPreview
         {
             public Vector3Int a;
             public Vector3Int b;
             public bool add;
-        }
+        }*/
 
         public void Setup(World world)
         {
@@ -57,8 +57,11 @@ namespace Mithmarie
         public override void OnTick()
         {
             base.OnTick();
-            if (firstPos != null && raycast.Cast(eyes, out hit))
-                secondPos = Utils.GetBlockPos(hit.point);
+            if (firstPos == null)
+                return;
+
+            raycast.Cast(eyes, out hit);
+            secondPos = Utils.GetBlockPos(hit.point);
 
             OnShowPreview?.Invoke(firstPos, secondPos);
         }
@@ -69,14 +72,14 @@ namespace Mithmarie
             if (ButtonPressed())
             {
                 ResetToDefault();
-                if (raycast.Cast(eyes, out hit))
-                    firstPos = Utils.GetBlockPos(hit.point);
+                raycast.Cast(eyes, out hit);
+                firstPos = Utils.GetBlockPos(hit.point);
             }
 
             if (firstPos != null && ButtonReleased())
             {
-                if (raycast.Cast(eyes, out hit))
-                    secondPos = Utils.GetBlockPos(hit.point);
+                raycast.Cast(eyes, out hit);
+                secondPos = Utils.GetBlockPos(hit.point);
 
                 OnEditSelection?.Invoke((Vector3Int)firstPos, (Vector3Int)secondPos);
                 world.Flush();
@@ -89,7 +92,5 @@ namespace Mithmarie
             firstPos = null;
             secondPos = null;
         }
-
-        public RaycastSettings GetSettings() => raycast;
     }
 }
