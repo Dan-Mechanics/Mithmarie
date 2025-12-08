1da77e9b72084ef06d06db8571808f092100b172
Dan-Mechanics <github.tim@desmidt.org>
2025-12-08 20:16:24 +0100

    Update BrushDisplay.cs

--- Mithmarie/Assets/Scripts/Player/BrushDisplay.cs
+++ Mithmarie/Assets/Scripts/Player/BrushDisplay.cs
@@ -1,41 +1,48 @@
-using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Mithmarie
 {
     public class BrushDisplay : MonoBehaviour
     {
-        [SerializeField] private TMP_Text centerText = default;
-        [SerializeField] private Color colorA = Color.white;
-        [SerializeField] private Color colorB = Color.white;
+        [SerializeField] private GameObject slotPrefab = default;
+        [SerializeField] private float perSlotOffset = default;
+        private Slot[] slots;
 
-        private KeyHighlight[] keyHighlights;
-
-        public void Setup()
+        public void Setup(int count, int index)
         {
-            keyHighlights = GetComponentsInChildren<KeyHighlight>();
-        }
-
-        private void Update()
-        {
-            for (int i = 0; i < keyHighlights.Length; i++)
+            slots = new Slot[count];
+            for (int i = 0; i < count; i++)
             {
-                keyHighlights[i].Draw(colorA, colorB);
-            }
-        }
+                RectTransform newSlot = Instantiate(slotPrefab, Vector3.zero, Quaternion.identity).GetComponent<RectTransform>();
+                newSlot.SetParent(transform);
+                newSlot.localPosition = Vector3.zero;
+                newSlot.localRotation = Quaternion.identity;
 
-        public void SetCenterText(HoverInformation hover) 
-        {
-            if(hover == null)
-            {
-                centerText.text = string.Empty;
-                return;
+                newSlot.anchoredPosition += i * perSlotOffset * Vector2.up;
+                slots[i].image = newSlot.GetComponent<Image>();
+                slots[i].arrow = newSlot.GetChild(0).gameObject;
             }
 
-            centerText.text = hover.pos.ToString();
+            ShowIndex(index);
+        }
+
+        public void ShowIndex(int index)
+        {
+            for (int i = 0; i < slots.Length; i++)
+            {
+                slots[i].arrow.SetActive(i == index);
+                slots[i].image.color = i == index ? Color.white : Color.gray;
+            }
         }
 
         public void Show() => gameObject.SetActive(true);
         public void Hide() => gameObject.SetActive(false);
+
+        private struct Slot
+        {
+            public Image image;
+            public GameObject arrow;
+        }
     }
 }
