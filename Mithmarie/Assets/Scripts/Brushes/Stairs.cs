6f93160835c4c4b28173ef02405a78a857d21c49
Dan-Mechanics <github.tim@desmidt.org>
2025-12-30 19:51:36 +0100

    Update Stairs.cs

--- Mithmarie/Assets/Scripts/Brushes/Stairs.cs
+++ Mithmarie/Assets/Scripts/Brushes/Stairs.cs
@@ -8,6 +8,11 @@ namespace Mithmarie
         private readonly Transform player;
         private readonly World world;
 
+        // I KNOW GLOBAL VARIABLES ARE NOT
+        // IDEAL BUT IT WOULD BE WORSE OTHERWISE.
+        private float slope;
+        private Vector3Int prev;
+
         public Stairs(World world, Transform player)
         {
             this.world = world;
@@ -35,83 +40,94 @@ namespace Mithmarie
                 direction = CardinalDirection.North;
 
             int width = Mathf.Abs(b.x - a.x) + 1;
+            int height = Mathf.Abs(b.y - a.y) + 1;
             int depth = Mathf.Abs(b.z - a.z) + 1;
 
             int xDirection = a.x <= b.x ? 1 : -1;
             int yDirection = a.y <= b.y ? 1 : -1;
             int zDirection = a.z <= b.z ? 1 : -1;
 
-            for (int y = 0; y < x + 1; y++)
+            Debug.Log(direction);
+            slope = (float)height / depth;
+
+            for (int x = 0; x < width; x++)
             {
                 for (int z = 0; z < depth; z++)
                 {
-
-                }
-            }
-
-
                     switch (direction)
-            {
-                case CardinalDirection.North:
-                    MakeNorthFacingStairs(a, add, width, depth, xDirection, yDirection, zDirection);
-                    break;
-                case CardinalDirection.East:
-                    MakeEastFacingStairs(a, add, width, depth, xDirection, yDirection, zDirection);
-                    break;
-                case CardinalDirection.South:
-                    MakeSouthFacingStairs(a, add, width, depth, xDirection, yDirection, zDirection);
-                    break;
-                case CardinalDirection.West:
-                    MakeWestFacingStairs(a, add, width, depth, xDirection, yDirection, zDirection);
-                    break;
-                default:
-                    throw new NotImplementedException();
+                    {
+                        case CardinalDirection.North:
+                            MakeNorthFacingStairs(a, add, x, z, xDirection, yDirection, zDirection);
+                            break;
+                        case CardinalDirection.East:
+                            MakeEastFacingStairs(a, add, x, z, xDirection, yDirection, zDirection);
+                            break;
+                        case CardinalDirection.South:
+                            MakeSouthFacingStairs(a, add, x, z, xDirection, yDirection, zDirection);
+                            break;
+                        case CardinalDirection.West:
+                            MakeWestFacingStairs(a, add, x, z, xDirection, yDirection, zDirection);
+                            break;
+                    }
+                }
             }
         }
 
         private void MakeNorthFacingStairs(Vector3Int a, bool add, int x, int z, int xDirection, int yDirection, int zDirection)
         {
-            for (int y = 0; y < z + 1; y++)
+            int max = z + 1;
+            for (int y = 0; y < max; y++)
             {
-                Place(a, add, xDirection, yDirection, zDirection, x, z, y);
+                Place(a, add, xDirection, yDirection, zDirection, x, y, z);
             }
         }
 
         private void MakeEastFacingStairs(Vector3Int a, bool add, int x, int z, int xDirection, int yDirection, int zDirection)
         {
-            for (int y = x; y >= 0; y--)
+            int max = x;
+            for (int y = max; y >= 0; y--)
             {
-                Place(a, add, xDirection, yDirection, zDirection, x, z, y);
+                Place(a, add, xDirection, yDirection, zDirection, x, y, z);
             }
         }
 
         private void MakeSouthFacingStairs(Vector3Int a, bool add, int x, int z, int xDirection, int yDirection, int zDirection)
         {
-            for (int y = z; y >= 0; y--)
+            int max = z;
+            for (int y = max; y >= 0; y--)
             {
-                Place(a, add, xDirection, yDirection, zDirection, x, z, y);
+                Place(a, add, xDirection, yDirection, zDirection, x, y, z);
             }
         }
 
         private void MakeWestFacingStairs(Vector3Int a, bool add, int x, int z, int xDirection, int yDirection, int zDirection)
         {
-            for (int y = 0; y < x + 1; y++)
+            int max = x + 1;
+            for (int y = 0; y < max; y++)
             {
-                Place(a, add, xDirection, yDirection, zDirection, x, z, y);
+                Place(a, add, xDirection, yDirection, zDirection, x, y, z);
             }
         }
 
-        private void Place(Vector3Int blockPos, bool add, int xDirection, int yDirection, int zDirection, int x, int z, int y)
+        private void Place(Vector3Int blockPos, bool add, int xDirection, int yDirection, int zDirection, int x, int y, int z)
         {
-            blockPos += new Vector3Int(x * xDirection, y * yDirection, z * zDirection);
-            if (add)
+            blockPos += new Vector3Int(x * xDirection, (int)(y * yDirection * slope), z * zDirection);
+            int height = Mathf.Abs(blockPos.y - prev.y);
+
+            for (int i = 0; i < height; i++)
             {
-                world.Add(blockPos);
+                if (add)
+                {
+                    world.Add(blockPos);
+                }
+                else
+                {
+                    world.Remove(blockPos);
+                }
+                blockPos.y++;
             }
-            else
-            {
-                world.Remove(blockPos);
-            }
+
+            prev = blockPos;
         }
     }
 }
