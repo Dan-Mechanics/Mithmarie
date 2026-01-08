7f12b8468cc1a1893dc05608e60e3783d911a696
Dan-Mechanics <github.tim@desmidt.org>
2026-01-08 23:26:24 +0100

    small changes

--- Mithmarie/Assets/Scripts/Brushes/Stairs.cs
+++ Mithmarie/Assets/Scripts/Brushes/Stairs.cs
@@ -72,14 +72,14 @@ namespace Mithmarie
         private bool IsBlockWithinStairsNorth(int width, int height, int depth, int x, int y, int z)
         {
             float slope = (float)height / depth;
-            int max = Mathf.RoundToInt(z * slope);
+            int max = Mathf.CeilToInt(z * slope);
             return y < max;
         }
 
         private bool IsBlockWithinStairsEast(int width, int height, int depth, int x, int y, int z)
         {
             float slope = (float)height / width;
-            int max = Mathf.RoundToInt(x * slope);
+            int max = Mathf.CeilToInt(x * slope);
             return y < max;
         }
 
